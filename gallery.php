c027adbdd8362ffe106958af205c425fbe41e3d5
Mike Peters <mapdesigns@gmail.com>
2014-05-14 19:08:55 -0400

    Initial commit

--- gallery.php
+++ gallery.php
@@ -139,11 +139,11 @@
 					<ul class="thumbnails cs-style-3">
 						<li>
 							<figure>
-								<a href="#"><img src="http://placehold.it/940x600/666666/ffffff" alt="img04"></a>
+								<a href="hazard"><img src="img/gallery-featured-hazard.jpg" alt="img04"></a>
 								<figcaption>
-									<h4>Settings</h4>
-									<span>A Description goes here</span>
-									<a href="#" class="btn-small inverse hide-for-small">View Project</a>
+									<h4>Hazard County</h4>
+									<span>McDonough, Ga</span>
+									<a href="hazard" class="btn-small inverse hide-for-small">View Project</a>
 								</figcaption>
 							</figure>
 						</li>
---
+++ hazard.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<!-- Start Header Section -->
+<?php include 'head.php'; ?>
+<!-- End Header Section -->
+
+<body>
+	<!-- Start Navigation Section -->
+	<?php include 'nav.php'; ?>
+	<!-- End Navigation Section -->
+
+	<!-- Start Header Section -->
+	<div class="header header_alt">
+
+		<div class="header-text">
+			<h2>Tabernacle</h2>
+			<h6 id="sub_header">Atlanta, Ga</h6>
+			<hr>
+		</div>
+		
+	</div>
+	<!-- End Header Section -->
+
+
+	<!-- Start Content Section -->
+
+	<div class="content content_alt">
+
+		<div class="bg_gradient">
+
+			<div class="row">
+	
+				<div class="large-3 push-9 columns">
+
+					<div class="sidebar">
+	
+						<h6>Client:</h6>
+						<h4>Tabernacle</h4>
+						<h6>Services:</h6>
+						<h4>Design</h4>
+						<h6>Date:</h6>
+						<h4>2013</h4>
+		
+						<a href="http://www.tabernacleatl.com" class="btn-medium btn-sidebar">More Info</a>
+
+					</div>
+	
+				</div>
+	
+				<div class="large-9 pull-3 columns">
+					<a href="img/gallery-tabernacle01.jpg" class="fancybox" rel="gallery"><img src="img/gallery-tabernacle01.jpg" class="portfolio-img"></a>
+					<a href="img/gallery-tabernacle02.jpg" class="fancybox" rel="gallery"><img src="img/gallery-tabernacle02.jpg" class="portfolio-img"></a>
+					<a href="img/gallery-tabernacle03.jpg" class="fancybox" rel="gallery"><img src="img/gallery-tabernacle03.jpg" class="portfolio-img-last"></a>
+				</div>
+	
+			</div>
+
+		</div>
+
+		<!-- Start Footer Section -->
+		<?php include 'footer.php'; ?>
+		<!-- End Footer Section -->
+	
+	</div>
+
+	<!-- End Content Section -->
+
+  <script>
+  document.write('<script src=js/vendor/' +
+  ('__proto__' in {} ? 'zepto' : 'jquery') +
+  '.js><\/script>')
+  </script>
+
+  <script src="js/foundation.min.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+
+  <script src="js/effects-alt.js"></script>
+
+</body>
+
+</html>
