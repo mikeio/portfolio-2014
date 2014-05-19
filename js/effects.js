cc23dd286e6d89ded739ec30c02b6b3213305086
Mike Peters <mapdesigns@gmail.com>
2014-05-18 22:26:45 -0400

    Adjust parallax

--- js/effects.js
+++ js/effects.js
@@ -31,7 +31,7 @@ jQuery('.header-text').css({
 
 // Call To Action
 jQuery('.bg_action').css({
-  'background-position-y' : 300+(scrollPos/4)+"px",
+  'background-position-y' : 225+(scrollPos/4)+"px",
 });
 
 }
