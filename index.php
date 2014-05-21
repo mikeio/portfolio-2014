e2bde44d33d494d100f5ceaae270e45e5f7cc551
Mike Peters <mapdesigns@gmail.com>
2014-05-21 12:36:03 -0400

    Remove ","

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	<div class="header">
 
 		<div class="header-text">
-			<h6 id="intro" class="animated fadeInUp">The Online Portfolio,&nbsp;of</h6>
+			<h6 id="intro" class="animated fadeInUp">The Online Portfolio&nbsp;of</h6>
 			<h1 class="animated fadeInUp">Mike Peters</h1>
 			<h6 id="outro" class="animated bounceIn">A designer from Atlanta,&nbsp;Ga</h6>
 			<hr class="animated fadeInUp">
