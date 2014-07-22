33826cb74ab8040d0175447ef6b60df314f16044
Mike Peters <mapdesigns@gmail.com>
2014-07-22 19:38:03 -0400

    Revert "Discard"
    This reverts commit cc9dc7d905315fb5f20001f2c3b85af204fc97f7.

--- myriad.php
+++ myriad.php
@@ -41,7 +41,9 @@
 						<p>Myriad is a cryptocurrency that uses five separate algorithms concurrently to create a Blockchain that focuses on technology and innovative usage.</p>
 						<p>With 30 second block times, Myriad is 20x faster than Bitcoin and one of the fastest cryptocurrencies in existence.</p>
 						<h6>Services:</h6>
-						<h5>Branding</h5>
+						<h4>Branding</h4>
+						<h6>Date:</h6>
+						<h4>2014</h4>
 		
 						<a href="http://myriadplatform.org/" class="btn-medium btn-sidebar">Myriad Platform</a>
 
