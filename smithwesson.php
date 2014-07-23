8b16252d86115deffb1716755230b13187381478
Mike Peters <mapdesigns@gmail.com>
2014-07-22 20:55:09 -0400

    Initial commit

---
+++ smithwesson.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<!-- Start Header Section -->
+<?php include 'head.php'; ?>
+<!-- End Header Section -->
+
+<body>
+	<!-- Start Navigation Section -->
+	<?php include 'nav.php'; ?>
+	<!-- End Navigation Section -->
+
+	<!-- Start Header Section -->
+	<div class="header header_alt">
+
+		<div class="header-text">
+			<h2>Smith & Wesson</h2>
+			<h6 id="sub_header">End Game Starts Here</h6>
+			<hr>
+		</div>
+		
+	</div>
+	<!-- End Header Section -->
+
+
+	<!-- Start Content Section -->
+
+	<div class="content content_alt">
+
+		<div class="bg_gradient">
+
+			<div class="row">
+	
+				<div class="large-3 push-9 columns">
+
+					<div class="sidebar">
+	
+						<h6 id="inverse">About:</h6>
+						<p>Smith & Wesson is a very versatile company, producing not only firearms but a wide variety of other items for the firearms and law enforcement communities.</p>
+						<p>The company's latest venture is into the footwear market with three robust duty boots: the Tac, Breach, and Hiker series.</p>
+						<hr>
+						<h6>Services:</h6>
+						<h4>Development</h4>
+						<h6>Date:</h6>
+						<h4>2014</h4>
+		
+						<a href="http://www.smithandwessonfootwear.com" class="btn-medium btn-sidebar">Launch Site</a>
+
+					</div>
+	
+				</div>
+	
+				<div class="large-9 pull-3 columns">
+					<a href="img/gallery-smithwesson01.jpg" class="fancybox" rel="gallery"><img src="img/gallery-smithwesson01.jpg" class="portfolio-img" alt="Smith & Wesson"></a>
+					<a href="img/gallery-smithwesson02.jpg" class="fancybox" rel="gallery"><img src="img/gallery-smithwesson02.jpg" class="portfolio-img-last" alt="Smith & Wesson"></a>
+				</div>
+	
+			</div>
+
+		</div>
+
+		<!-- Start Footer Section -->
+		<?php include 'footer.php'; ?>
+		<!-- End Footer Section -->
+	
+	</div>
+
+	<!-- End Content Section -->
+
+  <script>
+  document.write('<script src=js/vendor/' +
+  ('__proto__' in {} ? 'zepto' : 'jquery') +
+  '.js><\/script>')
+  </script>
+
+  <script src="js/foundation.min.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+
+  <script src="js/effects-alt.js"></script>
+
+</body>
+
+</html>
