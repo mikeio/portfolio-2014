b9bd19b43fa4dd3a4e2ba5c9ab70337a9559c572
Mike Peters <mapdesigns@gmail.com>
2014-10-30 22:39:51 -0400

    Add wow.js

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
 	<div class="header">
 
 		<div class="header-text">
-			<h6 id="intro" class="animated fadeInUp">The online portfolio&nbsp;of</h6>
-			<h1 class="animated fadeInUp">Mike Peters</h1>
-			<h6 id="outro" class="animated bounceIn">A visual designer from Atlanta,&nbsp;Ga</h6>
-			<hr class="animated fadeInUp">
+			<h6 id="intro" class="animated wow fadeInUp">The online portfolio&nbsp;of</h6>
+			<h1 class="animated wow fadeInUp">Mike Peters</h1>
+			<h6 id="outro" class="animated wow bounceIn">A visual designer from Atlanta,&nbsp;Ga</h6>
+			<hr class="animated wow fadeInUp">
 		</div>
 		
 	</div>
