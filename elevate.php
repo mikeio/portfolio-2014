cdfdc5d6079cdeca7f3ac15a0da504b781b1f827
Mike Peters <mapdesigns@gmail.com>
2015-02-25 16:09:05 -0500

    initial commit

---
+++ elevate.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<!-- Start Header Section -->
+<?php include 'head.php'; ?>
+<!-- End Header Section -->
+
+<body>
+	<!-- Start Navigation Section -->
+	<?php include 'nav.php'; ?>
+	<!-- End Navigation Section -->
+
+	<!-- Start Header Section -->
+	<div class="header header_alt">
+
+		<div class="header-text">
+			<h2>Elevate</h2>
+			<h6 id="sub_header">Hair Design</h6>
+			<hr>
+		</div>
+		
+	</div>
+	<!-- End Header Section -->
+
+
+	<!-- Start Content Section -->
+
+	<div class="content content_alt">
+
+		<div class="bg_gradient">
+
+			<div class="row">
+	
+				<div class="large-3 push-9 columns">
+
+					<div class="sidebar">
+
+						<h6 id="inverse">About:</h6>
+						
+						<p>Kemp Tennis Academy is an elite junior tennis training center and academic institution that is geared towards players who are committed to an intensive training schedule in an effort to maximize their potential both on and off the court.</p>
+						<p>Through a daily regimen of technical, tactical, mental, and physical training, Kemp Tennis Academy allows any player to reach the level of excellence they desire.</p>
+						<hr>
+						<h6>Services:</h6>
+						<h4>Web Design</h4>
+		
+						<a href="demo/elevate" class="btn-medium btn-sidebar">Elevate</a>
+
+					</div>
+	
+				</div>
+	
+				<div class="large-9 pull-3 columns">
+					<a href="img/gallery-elevate01.jpg" class="fancybox" rel="gallery"><img src="img/gallery-elevate01.jpg" class="portfolio-img" alt="Elevate Hair Design"></a>
+				</div>
+	
+			</div>
+
+			<div class="row">
+			
+				<div class="large-12 columns">
+					<a href="img/gallery-elevate02.jpg" class="fancybox wow fadeIn" rel="gallery"><img src="img/gallery-elevate02.jpg" class="portfolio-img-last" alt="Elevate Hair Design""></a>
+				</div>
+			
+			</div>
+
+		</div>
+
+		<!-- Start Footer Section -->
+		<?php include 'footer.php'; ?>
+		<!-- End Footer Section -->
+	
+	</div>
+
+	<!-- End Content Section -->
+
+  <script>
+  document.write('<script src=js/vendor/' +
+  ('__proto__' in {} ? 'zepto' : 'jquery') +
+  '.js><\/script>')
+  </script>
+
+  <script src="js/foundation.min.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+
+  <script src="js/effects-alt.js"></script>
+
+</body>
+
+</html>
