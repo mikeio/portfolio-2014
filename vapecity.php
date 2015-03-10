857ae784b36ff7c0c6bed68428ab044b250dc734
Mike Peters <mapdesigns@gmail.com>
2015-03-09 20:37:07 -0400

    initial commit

---
+++ vapecity.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<!-- Start Header Section -->
+<?php include 'head.php'; ?>
+<!-- End Header Section -->
+
+<body>
+	<!-- Start Navigation Section -->
+	<?php include 'nav.php'; ?>
+	<!-- End Navigation Section -->
+
+	<!-- Start Header Section -->
+	<div class="header header_alt">
+
+		<div class="header-text">
+			<h2>Vape City</h2>
+			<h6 id="sub_header">Austin, TX</h6>
+			<hr>
+		</div>
+		
+	</div>
+	<!-- End Header Section -->
+
+
+	<!-- Start Content Section -->
+
+	<div class="content content_alt">
+
+		<div class="bg_gradient">
+
+			<div class="row">
+	
+				<div class="large-3 push-9 columns">
+
+					<div class="sidebar">
+
+						<h6 id="inverse">About:</h6>
+						<p>Beginning with a humble start from a home office in July of 2012, Vape City has provided excellent service in the e-cig industry.</p>
+						<p>Vape City has quickly expanded from a home office, to garage, to rented warehouse space and recently to a 10,000 sq foot warehouse.</p>
+						<hr>
+						<h6>Services:</h6>
+						<h4>Branding</h4>
+		
+						<a class="btn-medium-inactive btn-sidebar">VapeCity</a>
+
+					</div>
+	
+				</div>
+	
+				<div class="large-9 pull-3 columns">
+				<a href="img/gallery-vapecity01.jpg" class="fancybox" rel="gallery"><img src="img/gallery-vapecity01.jpg" class="portfolio-img" alt="Vape City"></a>
+				</div>
+	
+			</div>
+
+			<div class="row">
+			
+				<div class="large-6 columns">
+					<a href="img/gallery-vapecity02.jpg" class="fancybox" rel="gallery"><img src="img/gallery-vapecity02.jpg" class="portfolio-img" alt="Vape City"></a>
+				</div>
+
+				<div class="large-6 columns">
+					<a href="img/gallery-vapecity03.jpg" class="fancybox" rel="gallery"><img src="img/gallery-vapecity03.jpg" class="portfolio-img" alt="Vape City"></a>
+				</div>
+			
+			</div>
+
+			<div class="row">
+			
+				<div class="large-12 columns">
+					<a href="img/gallery-vapecity04.jpg" class="fancybox" rel="gallery"><img src="img/gallery-vapecity04.jpg" class="portfolio-img" alt="Vape City"></a>
+				</div>
+			
+			</div>
+
+			<div class="row">
+			
+				<div class="large-12 columns">
+					<a href="img/gallery-vapecity05.jpg" class="fancybox" rel="gallery"><img src="img/gallery-vapecity05.jpg" class="portfolio-img-last" alt="Vape City"></a>
+				</div>
+			
+			</div>
+
+		</div>
+
+		<!-- Start Footer Section -->
+		<?php include 'footer.php'; ?>
+		<!-- End Footer Section -->
+	
+	</div>
+
+	<!-- End Content Section -->
+
+  <script>
+  document.write('<script src=js/vendor/' +
+  ('__proto__' in {} ? 'zepto' : 'jquery') +
+  '.js><\/script>')
+  </script>
+
+  <script src="js/foundation.min.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+
+  <script src="js/effects-alt.js"></script>
+
+</body>
+
+</html>
