d1208299ad418f6d648c86c1a2b23fbf37cf4d51
Mike Peters <mapdesigns@gmail.com>
2015-04-04 18:42:48 -0400

    initial commit

---
+++ atlanta.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<!-- Start Header Section -->
+<?php include 'head.php'; ?>
+<!-- End Header Section -->
+
+<body>
+	<!-- Start Navigation Section -->
+	<?php include 'nav.php'; ?>
+	<!-- End Navigation Section -->
+
+	<!-- Start Header Section -->
+	<div class="header header_alt">
+
+		<div class="header-text">
+			<h2>Atlanta's Skyline</h2>
+			<h6 id="sub_header">Personal Project</h6>
+			<hr>
+		</div>
+		
+	</div>
+	<!-- End Header Section -->
+
+
+	<!-- Start Content Section -->
+
+	<div class="content content_alt">
+
+		<div class="bg_gradient">
+
+			<div class="row">
+	
+				<div class="large-3 push-9 columns">
+
+					<div class="sidebar">
+
+						<h6 id="inverse">About:</h6>
+						<p>This is a vector illustration of Atlanta's skyline that I've been working on in my spare time.</p>
+						<p>Inspiration for this came from  Daniel Haire's <a href="https://dribbble.com/shots/1491353-Ferris-wheel-in-Atlanta">Ferris Wheel</a> and <a href="https://dribbble.com/shots/1681137-And-All-That-Glitters-An-Atl-Blog">All That Glitters</a> via Dribbble.
+						<hr>
+						<h6>Services:</h6>
+						<h4>Print Design</h4>
+		
+						<a href="#" class="btn-medium-inactive btn-sidebar">Learn More</a>
+
+					</div>
+	
+				</div>
+	
+				<div class="large-9 pull-3 columns">
+					<a href="img/gallery-atlanta01.jpg" class="fancybox" rel="gallery"><img src="img/gallery-atlanta01.jpg" class="portfolio-img" alt="Atlanta's Skyline"></a>
+				</div>
+	
+			</div>
+
+			<div class="row">
+			
+				<div class="large-12 columns">
+					<a href="img/gallery-atlanta02.jpg" class="fancybox wow fadeIn" rel="gallery"><img src="img/gallery-atlanta02.jpg" class="portfolio-img" alt="Atlanta's Skyline"></a>
+					<a href="img/gallery-atlanta03.jpg" class="fancybox wow fadeIn" rel="gallery"><img src="img/gallery-atlanta03.jpg" class="portfolio-img-last" alt="Atlanta's Skyline"></a>
+				</div>
+			
+			</div>
+
+		</div>
+
+		<!-- Start Footer Section -->
+		<?php include 'footer.php'; ?>
+		<!-- End Footer Section -->
+	
+	</div>
+
+	<!-- End Content Section -->
+
+  <script>
+  document.write('<script src=js/vendor/' +
+  ('__proto__' in {} ? 'zepto' : 'jquery') +
+  '.js><\/script>')
+  </script>
+
+  <script src="js/foundation.min.js"></script>
+
+  <script>
+    $(document).foundation();
+  </script>
+
+  <script src="js/effects-alt.js"></script>
+
+</body>
+
+</html>
